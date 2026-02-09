7df1bfdd17468cd27b1c334ab320dc8c984a4833
Priyanshu Jaiswal <jaiswalpriyanshu792@gmail.com>
2026-02-09 23:05:09 +0530

    ___

--- Lecture_4/Lecture_4_notes.py
+++ Lecture_4/Lecture_4_notes.py
@@ -82,7 +82,7 @@
 # Count how many unique letters there are in the string.
 
 # using string 
-# s = input("Enter your word: ")
+# s = (input("Enter your word: ")).lower()
 # alphabet = "" 
 # count = 0
 # for i in s:
@@ -93,7 +93,7 @@
         
 
 # #Using list 
-# s = input("Enter your word: ")
+# s = (input("Enter your word: ")).lower()
 # unique = []
 # for i in s:
 #     if i not in unique:
@@ -105,7 +105,7 @@
 # guess = 0
 # x = int(input("Enter the value of x: "))
 # if x<0:
-#     print(f"Did you really mean {x}????")
+#     print(f"Did you mean {-x}????")
 
 # if x>=0:
 #     while guess**2 < x:
@@ -144,31 +144,60 @@
 # How does the program look if I change the requirement to be If it's not found, 
 # prints that it didn't find it.
 
-num = 17
-#found = False
-for i in range(1,11):
-    if i == num:
-        print(i)
-else:
-    print("guess is out of range.")
+# num = 17
+# #found = False
+# for i in range(1,11):
+#     if i == num:
+#         print(i)
+# else:
+#     print("guess is out of range.")
 
 
+#GUESS-and-CHECK CUBE ROOT:POSITIVE CUBES using while loop
+# guess = 0 
+# x = int(input("Enter your number: "))
+# while guess**3<x:
+#     guess += 1
+# if guess**3 == x:
+#     print(f"{guess} is the cube root of {x}.")
+# else:
+#     print(f"{x} isn't a perfect cube number.")
+
+#GUESS-and-CHECK CUBE ROOT:POSITIVE CUBES using for loop
+
+# x = int(input("Enter your number: "))
+# abs_x = abs(x)
+# flag = False
+# # if x<0:
+    
+# for i in range(abs_x +1):
+#     if i**3 == abs_x :
+#         if x<0:
+#             print(f"{-i} is the cube root of {x}.")
+#         else:
+#             print(f"{i} is the cube root of {x}.")
+#         break
+# else:
+#     print(f"{x} isn't a perfect cube number.")
 
 
+# Remember those word problems from your childhood?
+#  For example:
+#  Alyssa, Ben, and Cindy are selling tickets to a fundraiser
+#  Ben sells 2 fewer than Alyssa
+#  Cindy sells twice as many as Alyssa
+#  10 total tickets were sold by the three people
+#  How many did Alyssa sell?
+#  Could solve this algebraically, but we can also use guess-and-check
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+total = 10
+for alyssa in range(11):
+    for ben in range(11):
+        for cindy in range(11):
+            if ben == alyssa - 2 and cindy == 2*alyssa and alyssa + ben + cindy == total:
+                print(f"Alysaa sold {alyssa} tickets.")
+                print(f"Ben sold {ben} tickets.")
+                print(f"Cindy sold {cindy} tickets.")
 
 
