52ea8e8357abef2107c08e4824e608166073d2e2
Priyanshu Jaiswal <jaiswalpriyanshu792@gmail.com>
2026-02-24 22:34:06 +0530

    Exercise

---
+++ Lecture_5/Exercise.py
@@ -0,0 +1,17 @@
+x = int(input("Enter a number: "))
+epsilon = 0.01
+num_guess = 0
+guess = 0.0
+increment = 0.001
+while abs(guess**2 - x) >= epsilon and guess**2<=x:
+    guess += increment
+    num_guess += 1
+    print(guess, guess**2, num_guess)
+print(f"num guesses = {num_guess}.")
+if abs (guess**2 - x) >= epsilon:
+    print(f"Failed to find square root of {x}.")
+    print(f"Last guess was {guess}.")
+    print(f"Last guess square was {guess**2}.")
+else:
+    print(f"{guess} is the closest to the square root of {x}.")
+    print(f"{guess**2} and {x**2}.")
