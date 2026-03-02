8718c3c32570633bfe3841229ba9b1800f319fcf
Priyanshu Jaiswal <jaiswalpriyanshu792@gmail.com>
2026-03-02 23:20:27 +0530

    Exercise solved

--- Lecture_6/Exercise.py
+++ Lecture_6/Exercise.py
@@ -1,4 +1,4 @@
-#Write a code to do the bisection search to find the cube root of postive cubes within some epsion.
+'''#Write a code to do the bisection search to find the cube root of postive cubes within some epsion.
 number: int = int(input("Enter the number: "))
 epsilon: float = 0.01
 low = 0
@@ -14,3 +14,28 @@ while abs(guess**3 - number)> epsilon:
     guess_count += 1
 print("The total number of guesses are %d."% (guess_count))
 print("{} is the closest to the cube root of the {}.".format(guess,number))
+'''
+
+
+
+#Assume you are given an integer 0 <= N <= 1000. 
+# Write a piece of Python code that uses bisection search to guess N. 
+# The code prints two lines: count: with how many guesses it took to find N, and answer: with the value of N. 
+# Hints: If the halfway value is exactly in between two integers, choose the smaller one.
+
+low = 0
+high = 1000
+N= 999
+guess_count = 0
+guess = (low + high)//2
+
+while guess != N:
+    guess_count += 1
+    if guess> N:
+        high = guess - 1
+    else:
+        low = guess + 1
+    guess = (low + high)//2
+    
+print(guess_count)
+print(guess)
--- Lecture_6/Lecture_6_notes.py
+++ Lecture_6/Lecture_6_notes.py
@@ -1,4 +1,4 @@
-x = float(input("Enter the number to find the square root of it: "))
+'''x = float(input("Enter the number to find the square root of it: "))
 epsilon = 0.01
 if  x<=1:
     low = x
@@ -16,4 +16,16 @@ while abs(guess**2 -x)>epsilon:
     guess = (low + high)/2
     guess_count += 1
 print(f"Total number of guesses is: {guess_count}")
-print(f"{guess} is the closest to the square root of {x}.")
+print(f"{guess} is the closest to the square root of {x}.")'''
+
+
+
+number = float(input("Enter a number: "))
+guess = number / 2.0
+guess_count = 0
+epsilon = 0.01
+while abs(guess*guess - number) >= epsilon:
+    guess_count += 1
+    guess = guess - (((guess**2) - number) / (2 * guess))
+print(guess_count)
+print(guess)
