51d0149d471d7591ed5d3931fa64c4fe0b40cb91
Priyanshu Jaiswal <jaiswalpriyanshu792@gmail.com>
2026-03-19 00:16:20 +0530

    Lecture 8 on the roll

--- Lecture_8/Lecture_8_notes.py
+++ Lecture_8/Lecture_8_notes.py
@@ -1,1 +1,9 @@
-print(print(5))
+heights = [1,1,4,2,1,3]
+count = 0
+expected = heights
+expected.sort()
+for i in range(len(heights)):
+    if  expected[i] != heights[i]:
+        count += 1
+print(count)
+print(expected)
