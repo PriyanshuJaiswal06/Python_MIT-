5e051a7965d584cbd4c258506235a8293d6e028e
Priyanshu Jaiswal <jaiswalpriyanshu792@gmail.com>
2026-04-02 22:28:37 +0530

    Lecture 9

--- Lecture_9/Lecture_9_notes.py
+++ Lecture_9/Lecture_9_notes.py
@@ -3,3 +3,13 @@ a = (lambda x:x%2 == 0)
 def do_twice(n, fn):
     return fn(fn(n))
 print(do_twice(3, lambda x: x**2))
+print((lambda c: c**2)(5))
+b = print("5")
+print(b)
+print(print("5"))
+def is_(n):
+    return(n**2)
+a = is_(5)
+print(a)
+
+is_(5)
